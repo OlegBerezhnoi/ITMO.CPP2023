f1f3d64d71fa77e8684e8af1ae761ddd27f0bc70
OlegBerezhnoi <oberezhnoy@ya.ru>
2023-06-13 17:58:33 +0300

    complete lesson 2 ex1

---
+++ ITMO.CPP2023.Lesson02.Ex1/ITMO.CPP2023.Lesson02.Ex1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+bool checkPrimeNumber(int number);
+
+int main()
+{
+    int checkingNumber;
+    bool isPrime;
+    int primeNumber = 0;
+
+    std::cout << "Enter number: ";
+    std::cin >> checkingNumber;
+
+    isPrime = checkPrimeNumber(checkingNumber);
+
+    if (isPrime)
+    {
+        for (int i = 2; i <= checkingNumber; i++)
+        {
+            isPrime = checkPrimeNumber(i);
+
+            if (isPrime)
+            {
+                primeNumber++;
+            }
+        }
+
+        isPrime = checkPrimeNumber(primeNumber);
+
+        if (isPrime)
+        {
+            std::cout << "Yes" << std::endl;
+        }
+        else
+        {
+            std::cout << "No" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "No" << std::endl;
+    }
+}
+
+bool checkPrimeNumber(int number)
+{
+    int counter = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            counter++;
+        }
+    }
+
+    if (counter == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
