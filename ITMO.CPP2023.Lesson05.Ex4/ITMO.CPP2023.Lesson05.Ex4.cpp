e46ff465f94d5346ebe5ba6a7b38ddb0554e43b8
OlegBerezhnoi <oberezhnoy@ya.ru>
2023-06-14 18:24:46 +0300

    L5ex4

---
+++ ITMO.CPP2023.Lesson05.Ex4/ITMO.CPP2023.Lesson05.Ex4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cstring>
+
+int main(int argc, char* argv[])
+{
+    
+    int firstVal;
+    int secondVal;
+
+    if (argc != 4)
+    {
+        std::cout << 0;
+        return 0;
+    }
+
+    firstVal = atoi(argv[2]);
+    secondVal = atoi(argv[3]);
+
+    if (firstVal == 0 || secondVal == 0)
+    {
+        std::cout << 0;
+        return 0;
+    }
+
+    if (strncmp(argv[1], "-a", 2) == 0)
+    {
+        std::cout << firstVal + secondVal;
+        return firstVal + secondVal;
+    }
+    else if (strncmp(argv[1], "-m", 2) == 0)
+    {
+        std::cout << firstVal * secondVal;
+        return firstVal * secondVal;
+    }
+    else
+    {
+        std::cout << 0;
+        return 0;
+    }
+}
