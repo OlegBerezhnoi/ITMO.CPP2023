47035f13b60b727227d4524d8acbb654ef0abdc5
OlegBerezhnoi <oberezhnoy@ya.ru>
2023-06-14 17:57:54 +0300

    L3ex4 commit

--- ITMO.CPP2023.Lesson03.Ex3/ITMO.CPP2023.Lesson03.Ex3.cpp
+++ ITMO.CPP2023.Lesson03.Ex3/ITMO.CPP2023.Lesson03.Ex3.cpp
@@ -1,20 +1,54 @@
-﻿// ITMO.CPP2023.Lesson03.Ex3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
 
-#include <iostream>
+double getTriangleArea(int side);
+double getTriangleArea(int firstSide, int secondSide, int thirdSide);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int triangleType;
+    double area;
+    int side1;
+    int side2;
+    int side3;
+
+    std::cout << "Select type of triangle (1 - equilateral, 2 - scalene): ";
+    std::cin >> triangleType;
+
+    if (triangleType == 1)
+    {
+        std::cout << "Enter side: ";
+        std::cin >> side1;
+        area = getTriangleArea(side1);
+    }
+    else if (triangleType == 2)
+    {
+        std::cout << "Enter three side:\n";
+        std::cin >> side1 >> side2 >> side3;
+        area = getTriangleArea(side1, side2, side3);
+    }
+    else
+    {
+        std::cout << "You entered wrong number of type";
+        return 0;
+    }
+
+    std::cout << "Area of a triangle is " << area;
+
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+double getTriangleArea(int side)
+{
+    double answer;
+    answer = (pow(3, 1.0 / 3) / 4) * side * side;
+    return answer;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+double getTriangleArea(int firstSide, int secondSide, int thirdSide)
+{
+    double answer;
+    double semiPerim = (firstSide + secondSide + thirdSide) / 2;
+    answer = sqrt(semiPerim * (semiPerim - firstSide) * (semiPerim - secondSide) * (semiPerim - thirdSide));
+    return answer;
+}
